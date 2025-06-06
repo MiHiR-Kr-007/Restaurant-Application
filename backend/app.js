268dafd21ef9d8d6d4615d18d2d35b9def2b5e4f
Mihir <mihir.singh2904@gmail.com>
2025-06-06 23:10:17 +0530

    cors error fixed

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 
 import { dbConnection } from "./database/dbConnection.js";
-import ReservationRouter from "./routes/reservationRoute.js";
+import router from "./routes/reservationRoute.js";
 import { errorMiddleWare } from "./middlewares/error.js";
 
 const app = express();
@@ -12,15 +12,15 @@ dotenv.config();
 app.use(
     cors({
         origin : [process.env.FRONTEND_URL],
-        method : ["POST"],
-        credentials : true
+        methods : ["POST"],
+        credentials : true,
     })
 );
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
-app.use("/api/reservation", ReservationRouter);
+app.use("/api/reservation", router);
 app.get("/", (req, res, next) => {
     return res.status(200).json({
         sucess : true,
